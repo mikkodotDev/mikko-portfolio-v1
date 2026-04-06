fb9fec05afb39af1e81c4ca2c9ad762c278a0d00
Mikko Jardenico <mjardenico_210000001699@uic.edu.ph>
2026-04-06 20:52:27 +0800

    Fixed base path

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
-  base: process.env.NODE_ENV === 'production' ? '/ReactWebPortfolio/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/MyPortfolio/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
